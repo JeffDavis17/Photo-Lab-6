38335cca41907a2cfc84bfde90b01b6728acecd1
JeffDavis17 <47422520+JeffDavis17@users.noreply.github.com>
2020-03-19 22:23:27 -0400

    initial est

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 from sympy import *
 import math as m
+from numpy.linalg import *
 
 
 # Data 
@@ -14,19 +15,11 @@ f = 101.4 # Focal Length in mm
 
 
 # Step 1 - Initial Values for the Ground Coordinates
-# Im_coords → g_coords
-
-# First Approximate the Ground Coordinates using Coplanarity
-
-
-
 
 # Baseline = L2 - L1
 B = np.array([[ex[1,0] - ex[0,0]],
 [ex[1,1] - ex[0,1]],
 [ex[1,2] - ex[0,2]]])
-
-
 
 
 # Rotation Matrices for im 1 and 2
@@ -44,10 +37,34 @@ def Rot(o,p,k):
     m31 = sin(p)
     m32 = -sin(o)*cos(p)
     m33 = cos(o)*cos(p)
-    M = np.array([[m11,m12,m13],[m21,m22,m23],[m31,m32,m33]])
+    M = np.array([[m11,m12,m13],[m21,m22,m23],[m31,m32,m33]],dtype=float)
     return M
 
 R1 = Rot(ex[0,3],ex[0,4],ex[0,5])
 R2 = Rot(ex[1,3],ex[1,4],ex[1,5])
+
+# Determine Lambda 
+pt1 = np.array([im_coord[0,0],im_coord[0,1],-f],dtype=float)
+pt2 = np.array([im_coord[1,0],im_coord[1,1],-f],dtype=float)
+pt3 = np.array([im_coord[2,0],im_coord[2,1],-f],dtype=float)
+u1 = R1@pt1
+u2 = R2@pt2
+
+a = np.array([[-u1[0],u2[0]],[-u1[1],u2[1]]])
+b = np.array([B[0],B[1]])
+lam = solve(a,b)
+
+# Determine initial values for the ground coordinates of the selected tie points:
+X1 = np.array([ex[0,0],ex[0,1],ex[0,2]],dtype=float) + lam[0]*R1@pt1
+X2 = np.array([ex[0,0],ex[0,1],ex[0,2]],dtype=float) + lam[0]*R1@pt2
+X3 = np.array([ex[0,0],ex[0,1],ex[0,2]],dtype=float) + lam[0]*R1@pt3
+
+
+# Step 2 - Observation Equations
+
+# r s and q terms:
+
+
+
 
 
